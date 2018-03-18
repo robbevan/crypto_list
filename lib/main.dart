f2bef5b933ded7b71c00696edc6e02d0951c1eb5
Rob Bevan <robbevan@xpt.com>
2018-03-18 10:03:53 +0000

    request and print CoinMarketCap response

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 // This is where the app starts executing.
-void main() {
-  print('Hello World!');
+void main() async {
+  // Testing the getCurrencies function
+  // We wait for the currency data to arrive
+  List currencies = await getCurrencies();
+  // Before printing it to the Console
+  print(currencies);
 
-  // Runs the MaterialApp widget
   runApp(new MaterialApp(
-    // This is the widget that is displayed first when the application is started normally
     home: new Center(
-      // The Text widget is wrapped in a center widget to center it on the screen
       child: new Text('Hello World!'),
     ),
   ));
+}
+
+Future<List> getCurrencies() async {
+  String apiUrl = 'https://api.coinmarketcap.com/v1/ticker/?limit=50';
+  // Make a HTTP GET request to the CoinMarketCap API.
+  // Await basically pauses execution until the get() function returns a Response
+  http.Response response = await http.get(apiUrl);
+  // Using the JSON class to decode the JSON String
+  return JSON.decode(response.body);
 }
