d1618e7c6109aa3becc0fcf1c831a57cd6054874
Rob Bevan <robbevan@xpt.com>
2018-03-18 10:00:05 +0000

    hello world widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
+import 'package:flutter/material.dart';
+
 // This is where the app starts executing.
 void main() {
-  print('Hello World'); // Prints Hello World! to the console
+  print('Hello World!');
+
+  // Runs the MaterialApp widget
+  runApp(new MaterialApp(
+    // This is the widget that is displayed first when the application is started normally
+    home: new Center(
+      // The Text widget is wrapped in a center widget to center it on the screen
+      child: new Text('Hello World!'),
+    ),
+  ));
 }
